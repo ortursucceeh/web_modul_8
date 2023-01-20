cd0a77f8a587f3159df5a0102f53c7b39ee8ba28
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-01-20 20:16:47 +0200

    describe models in mongo_db/models.py

---
+++ mongo_db/connect_db.py
@@ -0,0 +1,3 @@
+from mongoengine import connect
+
+connect(host=f"mongodb://127.0.0.1:27017/my_db")
---
+++ mongo_db/models.py
@@ -0,0 +1,19 @@
+from mongoengine import Document, CASCADE
+from mongoengine.fields import ListField, StringField, DateField, ReferenceField
+from mongoengine import connect
+
+connect(host=f"mongodb://127.0.0.1:27017/my_db")
+
+
+
+class Author(Document):
+    fullname = StringField(required=True, unique=True)
+    born_date = DateField()
+    born_location = StringField()
+    description = StringField()
+
+
+class Quote(Document):
+    tags = ListField(StringField(max_length=30))
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    quote = StringField()
